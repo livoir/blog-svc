b0d08ea4f469395b89737e6a56fc9b218617f3d3
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2024-10-19 21:55:27 +0700

    feat(post): create new post

--- internal/domain/post.go
+++ internal/domain/post.go
@@ -1,15 +1,20 @@
 package domain
 
+import "time"
+
 type Post struct {
-	ID      int64  `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type PostRepository interface {
 	GetByID(id int64) (*Post, error)
+	Create(post *Post) error
 }
 
 type PostUsecase interface {
 	GetByID(id int64) (*Post, error)
+	Create(post *Post) error
 }
--- internal/usecase/post_usecase.go
+++ internal/usecase/post_usecase.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "livoir-blog/internal/domain"
+import (
+	"livoir-blog/internal/domain"
+	"time"
+)
 
 type postUsecase struct {
 	postRepo domain.PostRepository
@@ -12,4 +15,9 @@ func NewPostUsecase(repo domain.PostRepository) domain.PostUsecase {
 
 func (u *postUsecase) GetByID(id int64) (*domain.Post, error) {
 	return u.postRepo.GetByID(id)
+}
+
+func (u *postUsecase) Create(post *domain.Post) error {
+	post.CreatedAt = time.Now()
+	return u.postRepo.Create(post)
 }
