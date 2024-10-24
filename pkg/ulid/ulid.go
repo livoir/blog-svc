9749c9a857d458276a099edbace31b0d77c5021a
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2024-10-24 09:24:01 +0700

    ci: setup github action CI

--- pkg/ulid/ulid.go
+++ pkg/ulid/ulid.go
@@ -1,13 +1,13 @@
 package ulid
 
 import (
-	"math/rand"
+	"crypto/rand"
 	"time"
 
 	"github.com/oklog/ulid/v2"
 )
 
-var entropy = ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+var entropy = ulid.Monotonic(rand.Reader, 0)
 
 func New() string {
 	return ulid.MustNew(ulid.Timestamp(time.Now()), entropy).String()
