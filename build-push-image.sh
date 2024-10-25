c68420dac53b95e7fc9f9c91e1cde574055a4e4a
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2024-10-25 14:38:14 +0700

    add quoting

--- build-push-image.sh
+++ build-push-image.sh
@@ -28,14 +28,14 @@ fi
 
 # Build the Docker image
 echo "Building Docker image ${FULL_IMAGE_NAME}"
-if ! docker build --platform linux/amd64 --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from $FULL_IMAGE_NAME -t $FULL_IMAGE_NAME .; then
+if ! docker build --platform linux/amd64 --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ${FULL_IMAGE_NAME} -t ${FULL_IMAGE_NAME} .; then
     echo "Error: Failed to build Docker image ${FULL_IMAGE_NAME}"
     exit 1
 fi
 
 # Login to the GitHub Container Registry
 echo "Logging in to the GitHub Container Registry"
-if ! timeout 30s sh -c "echo $GITHUB_ACCESS_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin"; then
+if ! timeout 60s sh -c "echo ${GITHUB_ACCESS_TOKEN} | docker login ghcr.io -u ${GITHUB_USER} --password-stdin"; then
     echo "Error: Failed to authenticate with GHCR"
     exit 1
 fi
@@ -43,7 +43,7 @@ trap 'echo "Logging out from GHCR"; docker logout ghcr.io' EXIT
 # Push the Docker image to the registry
 max_attempts=3
 attempt=1
-until docker push ${FULL_IMAGE_NAME}; do
+until docker push "${FULL_IMAGE_NAME}"; do
     if [ $attempt -eq $max_attempts ]; then
         echo "Failed to push Docker image ${FULL_IMAGE_NAME} after $max_attempts attempts"
         exit 1 
@@ -56,8 +56,11 @@ echo "Successfully pushed image to ${FULL_IMAGE_NAME}"
 
 # Verify the pushed image
 echo "Verifying the pushed image"
-if ! docker pull ${FULL_IMAGE_NAME} > /dev/null 2>&1; then
+if ! docker pull "${FULL_IMAGE_NAME}" > /dev/null 2>&1; then
     echo "Error: Failed to verify the pushed image ${FULL_IMAGE_NAME}. Pull test failed"
     exit 1
 fi
 echo "Successfully verified the pushed image ${FULL_IMAGE_NAME}"   
+
+# Cleanup verification image
+docker rmi "${FULL_IMAGE_NAME}" > /dev/null 2>&1 || true
