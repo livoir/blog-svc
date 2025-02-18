8e1b8af77025e75af279f3a7fdbc470f0146459e
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2025-02-18 21:31:05 +0700

    feat(cache): handle err redis

--- internal/repository/cache_repository_redis.go
+++ internal/repository/cache_repository_redis.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"context"
 	"livoir-blog/internal/domain"
+	"livoir-blog/pkg/logger"
 
 	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
 )
 
 type CacheRepositoryRedis struct {
@@ -16,22 +18,46 @@ func NewCacheRepositoryRedis(client *redis.Client) domain.CacheRepository {
 }
 
 func (c *CacheRepositoryRedis) Clear(ctx context.Context) error {
-	return c.Client.FlushAll(ctx).Err()
+	err := c.Client.FlushAll(ctx).Err()
+	if err != nil {
+		logger.Log.Error("Failed to clear cache: ", zap.Error(err))
+		return err
+	}
+	return nil
 }
 
 func (c *CacheRepositoryRedis) Delete(ctx context.Context, key string) error {
-	return c.Client.Del(ctx, key).Err()
+	err := c.Client.Del(ctx, key).Err()
+	if err != nil {
+		logger.Log.Error("Failed to delete key from cache: ", zap.String("key", key), zap.Error(err))
+		return err
+	}
+	return nil
 }
 
 func (c *CacheRepositoryRedis) Get(ctx context.Context, key string) (interface{}, error) {
-	return c.Client.Get(ctx, key).Result()
+	result, err := c.Client.Get(ctx, key).Result()
+	if err != nil {
+		logger.Log.Error("Failed to get value from cache: ", zap.String("key", key), zap.Error(err))
+		return nil, err
+	}
+	return result, nil
 }
 
 func (c *CacheRepositoryRedis) Has(ctx context.Context, key string) (bool, error) {
 	val, err := c.Client.Exists(ctx, key).Result()
-	return val > 0, err
+	if err != nil {
+		logger.Log.Error("Failed to check existence of key in cache: ", zap.String("key", key), zap.Error(err))
+		return false, err
+	}
+	return val > 0, nil
 }
 
 func (c *CacheRepositoryRedis) Set(ctx context.Context, key string, value interface{}) error {
-	return c.Client.Set(ctx, key, value, 0).Err()
+	err := c.Client.Set(ctx, key, value, 0).Err()
+	if err != nil {
+		logger.Log.Error("Failed to set value in cache: ", zap.String("key", key), zap.Error(err))
+		return err
+	}
+	return nil
 }
--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -2,18 +2,30 @@ package cache
 
 import (
 	"context"
+	"errors"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func NewRedisClient(address, password string, db int) *redis.Client {
+func NewRedisClient(address, password string, db int) (*redis.Client, error) {
 	client := redis.NewClient(&redis.Options{
 		Addr:     address,
 		Password: password,
 		DB:       db,
 	})
 	if _, err := client.Ping(context.Background()).Result(); err != nil {
-		return nil
+		return nil, errors.New("failed to connect to Redis: " + err.Error())
 	}
-	return client
+	return client, nil
+}
+
+func CloseRedisClient(client *redis.Client) error {
+	if client == nil {
+		return errors.New("client is nil")
+	}
+	err := client.Close()
+	if err != nil {
+		return errors.New("failed to close Redis client: " + err.Error())
+	}
+	return nil
 }
