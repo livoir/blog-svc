5c0e641da439788042ff26f5803c4b0f12bf5f45
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2025-02-20 22:12:05 +0700

    feat(cache): set expiration time

--- internal/domain/cache.go
+++ internal/domain/cache.go
@@ -1,9 +1,12 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type CacheRepository interface {
-	Set(ctx context.Context, key string, value interface{}) error
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
 	Get(ctx context.Context, key string) (interface{}, error)
 	Delete(ctx context.Context, key string) error
 	Clear(ctx context.Context) error
