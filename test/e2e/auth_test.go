f5a0338c7ad6c7577442724932d59f91f1400472
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2025-02-25 20:59:04 +0700

    test(auth): add integration test for google login redirect

---
+++ test/e2e/auth_test.go
@@ -0,0 +1,52 @@
+package e2e
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func (suite *E2ETestSuite) TestGoogleLoginRedirect() {
+	t := suite.T()
+
+	viper.Set("server.allowed_redirects", []string{"localhost:8081"})
+
+	testCases := []struct {
+		name           string
+		redirectUrl    string
+		expectedStatus int
+	}{
+		{
+			name:           "Google login without redirect",
+			redirectUrl:    "",
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Google login with invalid redirect",
+			redirectUrl:    "http://localhost:8080",
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Google login with valid redirect",
+			redirectUrl:    "http://localhost:8081",
+			expectedStatus: http.StatusTemporaryRedirect,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			req, _ := http.NewRequest("GET", "/auth/google/login", nil)
+			if tc.redirectUrl != "" {
+				q := req.URL.Query()
+				q.Add("redirect", tc.redirectUrl)
+				req.URL.RawQuery = q.Encode()
+			}
+			w := httptest.NewRecorder()
+			suite.router.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+		})
+	}
+}
