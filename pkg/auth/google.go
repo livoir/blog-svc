abc90743510b7cbf4f0784b4e0a4c60e7ff76e17
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2025-03-16 14:11:01 +0700

    feat(auth): login with discord

--- pkg/auth/google.go
+++ pkg/auth/google.go
@@ -1,11 +1,6 @@
 package auth
 
 import (
-	"context"
-	"encoding/json"
-	"livoir-blog/pkg/common"
-	"net/http"
-
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -14,14 +9,6 @@ import (
 var (
 	GoogleOauthConfig *oauth2.Config
 )
-
-type GoogleUser struct {
-	ID            string `json:"id"`
-	Email         string `json:"email"`
-	VerifiedEmail bool   `json:"verified_email"`
-	Name          string `json:"name"`
-	Picture       string `json:"picture"`
-}
 
 func NewGoogleOauthConfig() *oauth2.Config {
 	return &oauth2.Config{
@@ -34,23 +21,4 @@ func NewGoogleOauthConfig() *oauth2.Config {
 		},
 		Endpoint: google.Endpoint,
 	}
-}
-
-func GetUserInfo(token *oauth2.Token) (*GoogleUser, error) {
-	client := GoogleOauthConfig.Client(context.Background(), token)
-	response, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return nil, common.NewCustomError(response.StatusCode, "Failed to get user info")
-	}
-	user := &GoogleUser{}
-	if err := json.NewDecoder(response.Body).Decode(user); err != nil {
-		return nil, err
-	}
-
-	return user, nil
 }
