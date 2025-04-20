ce5993054e45df0deb377aa1981a0f500ade38c0
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2025-04-20 15:10:19 +0700

    feat(cache): add keydb pkg

---
+++ pkg/cache/keydb.go
@@ -0,0 +1,32 @@
+package cache
+
+import (
+	"context"
+	"errors"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewKeyDBClient(address, username, password string, db int) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: password,
+		DB:       db,
+		Username: username,
+	})
+	if _, err := client.Ping(context.Background()).Result(); err != nil {
+		return nil, errors.New("failed to connect to KeyDB: " + err.Error())
+	}
+	return client, nil
+}
+
+func CloseKeyDBClient(client *redis.Client) error {
+	if client == nil {
+		return errors.New("client is nil")
+	}
+	err := client.Close()
+	if err != nil {
+		return errors.New("failed to close KeyDB client: " + err.Error())
+	}
+	return nil
+}
